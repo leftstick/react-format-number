0564f092d39acd7bdae4612abd46126e00153b3d
howard.zuo <leftstick@qq.com>
2015-08-08 21:16:11 +0800

    update demo and docs

---
+++ demo.js
@@ -0,0 +1,37 @@
+(function(React, FormatNumber, $) {
+    'use strict';
+
+
+    var options = {
+        decimal: Number($('input[name="decimal"]:checked').val()),
+        previousValue: undefined
+    };
+
+    var valueChange = function(value) {
+        $('#value').val(value);
+        options.previousValue = value;
+    };
+
+    var component = React.render(<FormatNumber fractionSize={options.decimal} onChange={valueChange} value={options.previousValue}/>, $('#react-number')[0]);
+
+    $('#react-number input').on('focus', function() {
+        $(this).parent().siblings().addClass('active');
+    });
+    $('#react-number input').on('blur', function() {
+        $(this).parent().siblings().each(function() {
+            if ($('#react-number input').val() && $(this)[0].tagName.toUpperCase() === 'LABEL') {
+                return;
+            }
+            $(this).removeClass('active');
+        });
+    });
+
+    $('input[name="decimal"]').on('change', function() {
+        options.decimal = Number($(this).val());
+        component.setProps({
+            fractionSize: options.decimal,
+            value: options.previousValue
+        });
+    });
+
+}(React, window.FormatNumber, jQuery));
